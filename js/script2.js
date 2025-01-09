f2f2fcd1ab881144c6926554ce29053349cf8e35
lougo27 <louanegouin@gmail.com>
2025-01-09 16:35:52 +0100

    carte à gratter mobile

--- js/script2.js
+++ js/script2.js
@@ -4,10 +4,10 @@ const ctx = canvas.getContext("2d")
 
 let isMousePress = false
 
-//ctx.fillStyle = "red"
+ctx.fillStyle = "#EE9B00"
 ctx.fillRect(0,0,canvas.width,canvas.height)
-ctx.font = "20px League Spartan"
-ctx.fillStyle ="red"
+ctx.font = "22px League Spartan"
+ctx.fillStyle ="#CA6702"
 ctx.fillText("Grattez ici !", 100,140)
 
 let image = new Image();
@@ -24,6 +24,12 @@ function getCursorPosition(canvas, event) {
     return {x,y}
 }
 
+function mobilecheck() {
+    return (typeof window.orientation !== "undefined") 
+      || (navigator.userAgent.indexOf('IEMobile') !== -1
+      );
+};
+
 canvas.addEventListener('mousedown', function(){
     isMousePress = true
 })
@@ -33,12 +39,12 @@ window.addEventListener('mouseup', function(){
 })
 
 canvas.addEventListener('mousemove', function(e) {
-    if (isMousePress) {
+    if (isMousePress || mobilecheck()) {
         const position = getCursorPosition(canvas,e)
         //ctx.fillStyle = "#000000";
         ctx.globalCompositeOperation = 'destination-out';
         ctx.beginPath();
-        ctx.arc(position.x, position.y / 2, 10, 0, 2*Math.PI);
+        ctx.arc(position.x, position.y / 2, 15, 0, 2*Math.PI);
         ctx.fill();
     }
     
