02ab2b3fd3ae730b9a9f03388b84631ac764564e
lougo27 <louanegouin@gmail.com>
2025-02-13 11:15:52 +0100

    avancement_versionRapide

--- js/script.js
+++ js/script.js
@@ -2,20 +2,33 @@ const containerList = document.getElementsByClassName('container');
 
 
 let photoProjet = [
-    { nom: 'coquillage', src: 'coquillage.jpg', titre: 'projet secret', description : 'blabalabajanajz', conclusion : 'bye-bye' },
-    { nom: 'eau', src: 'test image planet.webp', titre : "l'eau c'est la vie", description : "mais après tout l'alcool c'est de l'eau sauf que l'alcool tue", conclusion : 'on va tous mourrir', image1 : 'test image planet.webp', image2 : 'test image planet.webp', image3 : 'test image planet.webp'},
-    { nom: 'canape', src: 'canape.jpg', titre : 'Je veux dormir', description : "faudrait installer un lit dans la classe", conclusion : "Je rêve d'une petite sieste" },
+    { nom: 'coquillage', src: 'GameMuseum.png', titre: 'projet secret', description : 'blabalabajanajz', conclusion : 'bye-bye', image1 : 'GameMuseum.png', image2 : 'GameMuseum2.png', image3 : 'GameMuseum3.png'},
+    { nom: 'eau', src: 'galeriePhotos2.png', titre : "l'eau c'est la vie", description : "mais après tout l'alcool c'est de l'eau sauf que l'alcool tue", conclusion : 'on va tous mourrir', image1 : 'galeriePhotos.png', image2 : 'galeriePhotos2.png', image3 : 'galeriePhotos3.png'},
+    { nom: 'canape', src: 'Montre_LouaneGouin-100.jpg', titre : 'Illustration de montre', description : "faudrait installer un lit dans la classe", conclusion : "Je rêve d'une petite sieste", image1 : 'montre_modele.png', image2 : 'Montre_LouaneGouin-100.jpg' , image3 : 'Montre_LouaneGouin2.jpg', },
     { nom: 'maze', src: 'maze.png', titre : 'The Maze Game', description : "jeu crée dans mon temps libre pour m'entrainer au code", image1 : 'TheMazeImage.png', image2 : 'soundtrapMaze.png'},
-    { nom: 'hibou', src: 'hibou test.png' },
-    { nom: 'photo', src: 'photo.jpeg' },
-    { nom: 'photo1', src: 'photo1.jpg' },
-    { nom: 'trousse', src: 'trousse.jpg' },
+    { nom: 'hibou', src: 'bubble.png', image1 : 'bubble.png', image2 : "anim_bubble.png", image3 : 'mockup_bubble.jpg' },
+    { nom: 'photo', src: 'LogoLigne.png', image1 : 'LogoLigne.png', image2 : 'teeShirtNoirDos.png', image3 : 'naviguation site.mp4' },
+    { nom: 'photo1', src: 'doctolib.png', image1 : 'logo_doctolib.png', image2 : 'doctolib.png', image3 : 'Phase_explo_projet_perso.pdf' },
+    { 
+      nom: 'trousse',
+      src: 'trousse.jpg',
+      medias: [
+        {
+          type: "video",
+          path: 'assets/pouet'
+        },
+        {
+          type: "img",
+          path: 'assets/prout'
+        }
+      ]
+    },
     { nom: 'clavier', src: 'clavier.jpg' },
     { nom: 'diorte', src: 'diorte.jpg' },
     { nom: 'baballe', src: 'baballe.jpg' },
     { nom: 'mp14', src: 'mp14.jpg' },
     { nom: 'coquillage', src: 'coquillage.jpg' },
-    { nom: 'canape', src: 'canape.jpg' },
+    { nom: 'canape', src: 'canape.jpg' }, 
     
     
 ]
@@ -23,33 +36,42 @@ let photoProjet = [
 for (let i = 0; i < photoProjet.length; i++) {
     
     let photos = document.createElement('img');
+    let divPhotos = document.createElement('div');
     let divContainer = document.createElement('div');
     let divEffect = document.createElement('div');
     let click = document.createElement('a');
 
-    photos.src = `assets/images/${photoProjet[i].src}`
+    photos.src = `assets/images/projets/${photoProjet[i].src}`
     photos.alt = photoProjet[i].nom
+
+    divPhotos.style.backgroundImage = `url("assets/images/projets/${photoProjet[i].src}")`
+    divPhotos.style.backgroundSize = "cover"
+    //divPhotos.style.width = "8rem"
+    //divPhotos.style.height = "8rem"
 
     click.onclick = function() {
       document.getElementById('titreModal').innerText = photoProjet[i].titre
       document.getElementById('one').innerText = photoProjet[i].description
       document.getElementById('conclusion').innerText = photoProjet[i].conclusion
-      document.getElementById('image1').src = `assets/images/${photoProjet[i].image1}`
-      document.getElementById('image2').src = `assets/images/${photoProjet[i].image2}`
-      document.getElementById('image3').src = `assets/images/${photoProjet[i].image3}`
+      document.getElementById('image1').src = `assets/images/projets/${photoProjet[i].image1}`
+      document.getElementById('image2').src = `assets/images/projets/${photoProjet[i].image2}`
+      let dernierElement = document.getElementById('image3').createElement(photoProjet[i].medias.type)
+      //dernierElement.createElement(photoProjet[i].medias.type)
+      dernierElement.src =  `assets/images/projets/${photoProjet[i].image3}`
+      //document.getElementById('image3').src = `assets/images/projets/${photoProjet[i].image3}`
     }
 
 
     divEffect.className = "effect"
-    photos.className = "photoTest"
+    divPhotos.className = "photoTest"
     divContainer.className = "container"
     click.href = "#id01"
 
     divContainer.appendChild(divEffect)
     divEffect.appendChild(click)
-    click.appendChild(photos)
+    click.appendChild(divPhotos)
     document.getElementById('tourne').appendChild(divContainer)
- 
+
 }
 
 
@@ -78,6 +100,5 @@ photoTests.forEach(photoTest => {
 
   });
 });
-
 
 
--- js/script3.js
+++ js/script3.js
@@ -1,15 +1,3 @@
-window.onload = () => 
-    window.scrollTo(0, document.body.scrollHeight);
-};
 
-window.onscroll = () => {
-    const rocket = document.getElementById("rocket");
-    const scrollPosition = window.scrollY;
-    const maxScroll = document.body.scrollHeight - window.innerHeight;
 
-    
-    const rocketOffset = (scrollPosition / maxScroll) * 100;
 
-    
-    rocket.style.transform = `translateX(-50%) translateY(${rocketOffset}px)`;
-};
