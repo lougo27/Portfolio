4441e0af22d5c6588aa8a2441acdbe276c30d004
lougo27 <louanegouin@gmail.com>
2025-02-13 15:34:51 +0100

    vignettes

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ const containerList = document.getElementsByClassName('container');
 
 
 let photoProjet = [
-    { nom: 'coquillage', src: 'GameMuseum.png', titre: 'projet secret', description : 'blabalabajanajz', conclusion : 'bye-bye', image1 : 'GameMuseum.png', image2 : 'GameMuseum2.png', image3 : 'GameMuseum3.png'},
-    { nom: 'eau', src: 'galeriePhotos2.png', titre : "l'eau c'est la vie", description : "mais après tout l'alcool c'est de l'eau sauf que l'alcool tue", conclusion : 'on va tous mourrir', image1 : 'galeriePhotos.png', image2 : 'galeriePhotos2.png', image3 : 'galeriePhotos3.png'},
-    { nom: 'canape', src: 'Montre_LouaneGouin-100.jpg', titre : 'Illustration de montre', description : "faudrait installer un lit dans la classe", conclusion : "Je rêve d'une petite sieste", image1 : 'montre_modele.png', image2 : 'Montre_LouaneGouin-100.jpg' , image3 : 'Montre_LouaneGouin2.jpg', },
-    { nom: 'maze', src: 'maze.png', titre : 'The Maze Game', description : "jeu crée dans mon temps libre pour m'entrainer au code", image1 : 'TheMazeImage.png', image2 : 'soundtrapMaze.png'},
-    { nom: 'hibou', src: 'bubble.png', image1 : 'bubble.png', image2 : "anim_bubble.png", image3 : 'mockup_bubble.jpg' },
-    { nom: 'photo', src: 'LogoLigne.png', image1 : 'LogoLigne.png', image2 : 'teeShirtNoirDos.png', image3 : 'naviguation site.mp4' },
-    { nom: 'photo1', src: 'doctolib.png', image1 : 'logo_doctolib.png', image2 : 'doctolib.png', image3 : 'Phase_explo_projet_perso.pdf' },
+    { nom: 'coquillage', src: 'projets/GameMuseum.png', titre: 'projet secret', description : 'blabalabajanajz', conclusion : 'bye-bye', image1 : 'GameMuseum.png', image2 : 'GameMuseum2.png', image3 : 'GameMuseum3.png'},
+    { nom: 'eau', src: 'vignettes/Memory-100.jpg', titre : "l'eau c'est la vie", description : "mais après tout l'alcool c'est de l'eau sauf que l'alcool tue", conclusion : 'on va tous mourrir', image1 : 'galeriePhotos.png', image2 : 'galeriePhotos2.png', image3 : 'galeriePhotos3.png'},
+    { nom: 'canape', src: 'vignettes/montre-100.jpg', titre : 'Illustration de montre', description : "faudrait installer un lit dans la classe", conclusion : "Je rêve d'une petite sieste", image1 : 'montre_modele.png', image2 : 'Montre_LouaneGouin-100.jpg' , image3 : 'Montre_LouaneGouin2.jpg', },
+    { nom: 'maze', src: 'projets/maze.png', titre : 'The Maze Game', description : "jeu crée dans mon temps libre pour m'entrainer au code", image1 : 'TheMazeImage.png', image2 : 'soundtrapMaze.png'},
+    { nom: 'hibou', src: 'vignettes/bubble-100.jpg', image1 : 'bubble.png', image2 : "anim_bubble.png", image3 : 'mockup_bubble.jpg' },
+    { nom: 'photo', src: 'vignettes/happy-100.jpg', image1 : 'LogoLigne.png', image2 : 'teeShirtNoirDos.png', image3 : 'naviguation site.mp4' },
+    { nom: 'photo1', src: 'projets/doctolib.png', image1 : 'logo_doctolib.png', image2 : 'doctolib.png', image3 : 'Phase_explo_projet_perso.pdf' },
     { 
       nom: 'trousse',
       src: 'trousse.jpg',
@@ -23,13 +23,27 @@ let photoProjet = [
         }
       ]
     },
-    { nom: 'clavier', src: 'clavier.jpg' },
-    { nom: 'diorte', src: 'diorte.jpg' },
-    { nom: 'baballe', src: 'baballe.jpg' },
-    { nom: 'mp14', src: 'mp14.jpg' },
-    { nom: 'coquillage', src: 'coquillage.jpg' },
-    { nom: 'canape', src: 'canape.jpg' }, 
-    
+    { nom: 'coquillage', src: 'projets/GameMuseum.png', titre: 'projet secret', description : 'blabalabajanajz', conclusion : 'bye-bye', image1 : 'GameMuseum.png', image2 : 'GameMuseum2.png', image3 : 'GameMuseum3.png'},
+    { nom: 'eau', src: 'vignettes/Memory-100.jpg', titre : "l'eau c'est la vie", description : "mais après tout l'alcool c'est de l'eau sauf que l'alcool tue", conclusion : 'on va tous mourrir', image1 : 'galeriePhotos.png', image2 : 'galeriePhotos2.png', image3 : 'galeriePhotos3.png'},
+    { nom: 'canape', src: 'vignettes/montre-100.jpg', titre : 'Illustration de montre', description : "faudrait installer un lit dans la classe", conclusion : "Je rêve d'une petite sieste", image1 : 'montre_modele.png', image2 : 'Montre_LouaneGouin-100.jpg' , image3 : 'Montre_LouaneGouin2.jpg', },
+    { nom: 'maze', src: 'projets/maze.png', titre : 'The Maze Game', description : "jeu crée dans mon temps libre pour m'entrainer au code", image1 : 'TheMazeImage.png', image2 : 'soundtrapMaze.png'},
+    { nom: 'hibou', src: 'vignettes/bubble-100.jpg', image1 : 'bubble.png', image2 : "anim_bubble.png", image3 : 'mockup_bubble.jpg' },
+    { nom: 'photo', src: 'vignettes/happy-100.jpg', image1 : 'LogoLigne.png', image2 : 'teeShirtNoirDos.png', image3 : 'naviguation site.mp4' },
+    { nom: 'photo1', src: 'projets/doctolib.png', image1 : 'logo_doctolib.png', image2 : 'doctolib.png', image3 : 'Phase_explo_projet_perso.pdf' },
+    { 
+      nom: 'trousse',
+      src: 'trousse.jpg',
+      medias: [
+        {
+          type: "video",
+          path: 'assets/pouet'
+        },
+        {
+          type: "img",
+          path: 'assets/prout'
+        }
+      ]
+    },
     
 ]
 
@@ -41,10 +55,10 @@ for (let i = 0; i < photoProjet.length; i++) {
     let divEffect = document.createElement('div');
     let click = document.createElement('a');
 
-    photos.src = `assets/images/projets/${photoProjet[i].src}`
-    photos.alt = photoProjet[i].nom
+    // photos.src = `assets/images/vignettes/${photoProjet[i].src}`
+    // photos.alt = photoProjet[i].nom
 
-    divPhotos.style.backgroundImage = `url("assets/images/projets/${photoProjet[i].src}")`
+    divPhotos.style.backgroundImage = `url("assets/images/${photoProjet[i].src}")`
     divPhotos.style.backgroundSize = "cover"
     //divPhotos.style.width = "8rem"
     //divPhotos.style.height = "8rem"
